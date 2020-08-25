3d99298a15474ffab7ebe80d4a1adb32917f8311
Trung Ly <trung.ly@yoobeecolleges.com>
2020-08-26 09:22:41 +1200

    Adding Project component and sample data

--- src/RouteProjects.js
+++ src/RouteProjects.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Project from './Project';
 
 class RouteProjects extends Component {
 
@@ -6,15 +7,15 @@ class RouteProjects extends Component {
     super(props)
     this.state = {
       projects:[
-        // {
-        //   id:1,
-        //   name:'Build a hut',
-        //   description: 'Nice project'
-        // },{
-        //   id:2,
-        //   name:'Make a basket',
-        //   description: 'Pretty project'
-        // }
+        {
+          id:1,
+          name:'Build a hut',
+          description: 'Nice project'
+        },{
+          id:2,
+          name:'Make a basket',
+          description: 'Pretty project'
+        }
       ]
     }
   }
@@ -23,18 +24,17 @@ class RouteProjects extends Component {
     return (
       <div class="main">
         <h3>All projects</h3>
-        <div class="card project">
-          <img class="card-img-top" src="project.jpg" alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title</p>
-            <p>
-              <i class="fas fa-heart"></i>
-              <i class="fas fa-edit"></i>
-              <i class="fas fa-trash"></i>
-            </p>
-          </div>
-        </div>
+        {
+          this.state.projects.map((project) => {
+
+            var projectProps = {
+              ...project,
+              key: project.id,
+    
+            };
+            return (<Project {...projectProps} />)
+          })
+        }
       </div>
     )
   }
